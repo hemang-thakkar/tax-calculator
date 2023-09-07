644e1df12d906bf85a216289e8e0bec6e6fe0056
Hemang Thakkar <hemangpthakkar@gmail.com>
2023-09-07 10:26:14 -0400

    move fetch call within try catch block

--- src/data/api.ts
+++ src/data/api.ts
@@ -7,19 +7,25 @@ export const fetchTaxRates = async (
 ): Promise<TaxRatesResponse> => {
     const requestUrl = `${BASE_URL}/tax-year/${taxYear}`;
 
-    const response = await fetch(requestUrl);
-    const { tax_brackets }: TaxRatesJSONResponse = await response.json();
+    try {
+        const response = await fetch(requestUrl);
+        const { tax_brackets }: TaxRatesJSONResponse = await response.json();
 
-    if (response.ok && tax_brackets) {
-        return { tax_brackets };
-    } else {
-        if (response.status === 404) {
+        if (response.ok && tax_brackets) {
+            return { tax_brackets };
+        } else {
+            if (response.status === 404) {
+                return Promise.reject(
+                    new Error(
+                        `Tax year (${taxYear}) is not supported. Please check your tax year.`
+                    )
+                );
+            }
             return Promise.reject(
-                new Error(
-                    `Tax year (${taxYear}) is not supported. Please check your tax year.`
-                )
+                new Error('Unknown Server Error. Please try again')
             );
         }
+    } catch(err) {
         return Promise.reject(
             new Error('Unknown Server Error. Please try again')
         );
